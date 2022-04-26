4073fea98d66128d3740acf03bc67ab090562c5d
abdou-1614 <kirakira1614@gmail.com>
2022-04-26 16:55:30 -0700

    'two'

---
+++ components/HeroBanner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroBanner = () => {
+  return (
+    <div className='py-32 px-10 bg-[#dcdcdc] relative rounded-lg h-[500px] leading-[0.9] w-full'>
+      <div>
+        <p className='text-3xl'>SMALL TEXT </p>
+      </div>
+    </div>
+  )
+}
+
+export default HeroBanner
