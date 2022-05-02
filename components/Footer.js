3e1b0c84746d0381f23ee729a4d0a65899d23723
abdou-1614 <kirakira1614@gmail.com>
2022-05-01 18:28:04 -0700

    'end'

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import {AiFillInstagram, AiOutlineTwitter, AiFillFacebook} from 'react-icons/ai'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='flex text-2xl flex-col items-center py-8 px-3 mt-7 text-center text-[#324d67] justify-center gap-2 font-bold ' >
+      <p>2022 JS Gaming All Rights Reserved</p>
+      <p className='gap-8 text-4xl flex mt-2' >
+        <AiFillInstagram/>
+        <AiFillFacebook/>
+        <AiOutlineTwitter/>
+      </p>
+    </div>
   )
 }
 
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,18 @@
+import Link from 'next/link'
 import React from 'react'
-
+import {BiShoppingBag} from 'react-icons/bi'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex justify-between my-2 mx-4 relative '>
+        <p className='text-3xl font-bold text-gray-500'>
+          <Link href='/'>JS Gaming</Link>
+        </p>
+        <button type='button' className='text-[27px] text-gray-500 cursor-pointer relative bg-transparent border-none transition transform delay-100 ease hover:scale-110 '>
+          <BiShoppingBag/>
+          <span className="absolute w-[20px] h-[20px] right-[-7px] top-[-1px] text-center bg-red-500 text-white font-bold rounded-full text-[12px] " >1</span>
+        </button>
+    </div>
   )
 }
 
-export default Navbar
+export default Navbar;
